a499ada5d3c4189e83ace49d262464e30322d5fb
nikonyrh <niko.nyrhila@gmail.com>
2018-08-28 15:44:08 +0300

    Support for spacebar, refactorings

--- src/cljs/reagent_helloworld/core.cljs
+++ src/cljs/reagent_helloworld/core.cljs
@@ -13,29 +13,58 @@
     (str (apply str (repeat (- len (count i)) 0))
          i)))
 
+(defn ns-to-str [tot]
+  (let [tot (quot tot 1000000)
+        h   (quot tot 3600000) tot (- tot (* h 3600000))
+        m   (quot tot   60000) tot (- tot (* m   60000))
+        s   (quot tot    1000)  ms (- tot (* s    1000))]
+    (apply str (interleave
+                 (map to-fixed [2 2 2 3] [h m s ms])
+                 [":" ":" "." ""]))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(def state
+(def timer-states
   {:timer-state (atom :stopped)
    :down-ts     (atom 0)
    :start-ts    (atom 0)
-   :stop-ts     (atom 0)
-   :timer-str   (atom "00:00:00.000")
-   :times       (atom [])})
+   :stop-ts     (atom 0)})
 
-(defn deref-state [] (zipmap (keys state) (map deref (vals state))))
+(def result-states
+  {:times       (atom [])
+   :min-time    (atom nil)})
 
-(let [{:keys [timer-state down-ts start-ts stop-ts timer-str times]} state]
+
+(defn deref-states []
+  (->> (for [s [timer-states result-states]]
+         (zipmap (keys s) (map deref (vals s))))
+       (apply merge)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(let [{:keys [timer-state down-ts start-ts stop-ts times]} timer-states
+      {:keys [times min-time]} result-states]
   (defn tick! []
     (when (= @timer-state :started)
       (reset! stop-ts (reagent-helloworld.util/nanoTime))))
   
+  
+  (defn conj-time! [ts]
+    (reset! timer-state :stopped)
+    (reset! stop-ts ts)
+    (let [took-ns (- ts @start-ts)]
+      (swap! times conj took-ns)
+      (swap! min-time #(if % (min % took-ns) took-ns))))
+  
+  
   (let [tick-handle  (atom nil)
         reset-handle (atom nil)]
     (defn click! [event]
-      (js/console.log (str @timer-state " " event))
+      ; (js/console.log (str @timer-state " " event))
       (let [ts (reagent-helloworld.util/nanoTime)]
         (case [@timer-state event]
-          [:stopped :mouse-down]
+          [:stopped :up]   nil ; Mouse up when we stop the clock
+          [:pending :down] nil ; Holding down a spacebar
+            
+          [:stopped :down]
           (do (reset! down-ts ts)
               (reset! timer-state :pending)
               (when @reset-handle
@@ -45,50 +74,48 @@
                      #(when (= @timer-state :pending)
                         (doseq [a [down-ts start-ts stop-ts]] (reset! a 0))) 1000)
                    (reset! reset-handle)))
-          
-          [:stopped :mouse-up] nil
-          
-          [:pending :mouse-up]
+            
+          [:pending :up]
           (if (> @down-ts 0)
             (do (reset! timer-state :stopped))
             (do (reset! start-ts ts)
                 (reset! stop-ts  ts)
                 (reset! timer-state :started)
                 (reset! tick-handle (js/setInterval tick! 7))))
-          
-          [:started :mouse-down]
-          (do (reset! timer-state :stopped)
-              (reset! stop-ts ts)
-              (swap!  times conj (- ts @start-ts))
+            
+          [:started :down]
+          (do (conj-time! ts)
               (js/clearInterval @tick-handle)
-              (reset! tick-handle nil))
-          
-          [:started :mouse-up] nil))))
-  
-  (add-watch start-ts :start-ts-watch (fn [k r o n] (reset! stop-ts n)))
-  (add-watch stop-ts  :stop-ts-watch
-    (fn [k r o n]
-      (let [tot (quot (- @stop-ts @start-ts) 1000000)
-            h   (quot tot 3600000) tot (- tot (* h 3600000))
-            m   (quot tot   60000) tot (- tot (* m   60000))
-            s   (quot tot    1000)  ms (- tot (* s    1000))
-            [h m s ms] (map to-fixed [2 2 2 3] [h m s ms])]
-        (reset! timer-str (str h ":" m ":" s "." ms))))))
+              (reset! tick-handle nil)))))))
 
 
 (comment
   (deref-state))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(let [{:keys [timer-state down-ts start-ts timer-str]} state
-      padding "60px"]
+(let [{:keys [timer-state down-ts start-ts stop-ts]} timer-states
+
+      events (atom (list "")) click! #(do (swap! events conj [% (reagent-helloworld.util/nanoTime)])
+                                          (click! %))
+
+      padding "60px" spacebar? #(= (.-which %) 32)
+      make-event-fn #(fn[e] (when (% e) (click! %2)))
+      on-mouse-down (make-event-fn identity  :down)
+      on-mouse-up   (make-event-fn identity  :up)
+      on-key-down   (make-event-fn spacebar? :down)
+      on-key-up     (make-event-fn spacebar? :up)]
+
   (defn home-page []
-    [:div {:on-mouse-down #(click! :mouse-down)
-           :on-mouse-up   #(click! :mouse-up)
-           :style {:text-align "center" :class "test" :border "1px solid #8DF" :user-select "none";
+    [:div {:tab-index 0
+           :on-mouse-down on-mouse-down :on-mouse-up on-mouse-up
+           :on-key-down   on-key-down   :on-key-up   on-key-up
+           :touchstart    #(click! :down) :touchend #(click! :up)
+           :style {:text-align "center" :class "test" :border "4px solid #8DF" :user-select "none"
                    :padding-top padding :padding-bottom padding :margin padding
                    :color (cond (= @timer-state :pending) (if (= @down-ts 0) "green" "red") :else "black")}}
-     [:h1 @timer-str]]))
+     [:h1 (str (deref-states))]
+     [:h1 (str (clojure.string/join "/" (take 3 @events)))]
+     [:h1 (str @timer-state " " (ns-to-str (- @stop-ts @start-ts)))]]))
 
 
 (defn mount-root []
