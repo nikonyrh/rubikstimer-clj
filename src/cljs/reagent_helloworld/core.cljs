4636d291a2f2e389f18240091c8d2b7074bdb401
nikonyrh <niko.nyrhila@gmail.com>
2018-09-10 19:26:39 +0300

    Tweaks

--- src/cljs/reagent_helloworld/core.cljs
+++ src/cljs/reagent_helloworld/core.cljs
@@ -10,17 +10,17 @@
 
 (defn to-fixed [len i]
   (let [i (str i)]
-    (str (apply str (repeat (- len (count i)) 0))
+    (str (clojure.string/join (repeat (- len (count i)) 0))
          i)))
 
 (defn ns-to-str [tot]
   (let [tot (quot tot 1000000)
       ; h   (quot tot 3600000) tot (- tot (* h 3600000))
-        m   (quot tot   60000) tot (- tot (* m   60000))
+      ; m   (quot tot   60000) tot (- tot (* m   60000))
         s   (quot tot    1000)  ms (- tot (* s    1000))]
     (apply str (interleave
-                 (map to-fixed [2 2 3] [m s ms])
-                 [":" "." ""]))))
+                 (map to-fixed [2 2] [s (quot ms 10)])
+                 ["." ""]))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defonce timer-states
@@ -30,7 +30,7 @@
    :stop-ts     (atom 0)})
 
 (defonce result-states
-  {:times       (atom []) ;(vec (for [_ (range 20)] 0)))
+  {:times       (atom (vec (for [_ (range 15)] (-> (Math/random) (* 5) (+ 20) (* 1e9) long))))
    :min-time    (atom nil)
    :result-avgs (atom {5 nil 12 nil})})
 
@@ -49,6 +49,8 @@
     (for [i [3 5 10 12]]
       [i (avg-of-n data i)])))
 
+(def min-took-ns 2e9)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (let [{:keys [timer-state down-ts start-ts stop-ts times]} timer-states
       {:keys [times min-time result-avgs]} result-states]
@@ -61,18 +63,19 @@
     (reset! timer-state :stopped)
     (reset! stop-ts ts)
     (let [took-ns (- ts @start-ts)]
-      (swap! min-time #(if % (min % took-ns) took-ns))
-      (let [times  (swap! times conj took-ns)
-            avgs   @result-avgs
-            n-avgs (keys avgs)]
-        (reset! result-avgs
-          (zipmap n-avgs (for [n n-avgs] (avg-of-n times n)))))))
+      (when (> took-ns min-took-ns)
+        (swap! min-time #(if % (min % took-ns) took-ns))
+        (let [times  (swap! times conj took-ns)
+              avgs   @result-avgs
+              n-avgs (keys avgs)]
+          (reset! result-avgs
+            (zipmap n-avgs (for [n n-avgs] (avg-of-n times n))))))))
   
   
   (let [tick-handle  (atom nil)
         reset-handle (atom nil)]
     (defn click! [event]
-      ; (js/console.log (str @timer-state " " event))
+      (js/console.log (str @timer-state " " event))
       (let [ts (reagent-helloworld.util/nanoTime)]
         (case [@timer-state event]
           [:stopped :up]   nil ; Mouse up when we stop the clock
@@ -86,7 +89,7 @@
                 (reset! reset-handle nil))
               (->> (js/setTimeout
                      #(when (= @timer-state :pending)
-                        (doseq [a [down-ts start-ts stop-ts]] (reset! a 0))) 100)
+                        (doseq [a [down-ts start-ts stop-ts]] (reset! a 0))) 333)
                    (reset! reset-handle)))
           
           [:pending :up]
@@ -98,10 +101,10 @@
                 (reset! tick-handle (js/setInterval tick! 7))))
           
           [:started :down]
-          (when (-> (- ts @start-ts) (> 0.1e9))
-            (conj-time! ts)
+          (do
             (js/clearInterval @tick-handle)
-            (reset! tick-handle nil)))))))
+            (reset! tick-handle nil)
+            (conj-time! ts)))))))
 
 
 (comment
@@ -119,16 +122,21 @@
       on-key-up     (make-event-fn spacebar? :up)]
   
   (defn home-page []
-    [:div
-     [:div {:id "app-sidebar"}
-      [:ul (for [t @times] [:li (ns-to-str t)])]]
-     
-     [:div {:tab-index 0 :id "app-timer"
-            :on-mouse-down  on-mouse-down :on-mouse-up  on-mouse-up
-            :on-touch-start on-mouse-down :on-touch-end on-mouse-up
-            :on-key-down    on-key-down   :on-key-up    on-key-up
-            :style {:color (cond (= @timer-state :pending) (if (= @down-ts 0) "green" "red") :else "black")}}
-      [:h1 (ns-to-str (- @stop-ts @start-ts))]]]))
+    (let [time-ns (- @stop-ts @start-ts)]
+      [:div
+       [:div {:id "app-sidebar"}
+        [:ul (for [[ix t] (map list (range) @times)]
+               ^{:key ix} [:li (ns-to-str t)])]]
+       
+       [:div {:tab-index 0 :id "app-timer"
+              :on-mouse-down  on-mouse-down :on-mouse-up  on-mouse-up
+              :on-touch-start on-mouse-down :on-touch-end on-mouse-up
+              :on-key-down    on-key-down   :on-key-up    on-key-up
+              :style {:color (cond
+                               (= @timer-state :pending) (if (= @down-ts 0) "green" "red")
+                               (= @timer-state :stopped) (if (> time-ns min-took-ns) "black" "gray")
+                               :else "black")}}
+        [:h1 (ns-to-str time-ns)]]])))
      
 
 
