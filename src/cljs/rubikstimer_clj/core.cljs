dba6a65d6ce53dfd666cd379a5f044f233993be0
nikonyrh <niko.nyrhila@gmail.com>
2018-10-01 19:30:12 +0300

    Move to a new namespace

--- src/cljs/rubikstimer_clj/core.cljs
+++ src/cljs/rubikstimer_clj/core.cljs
@@ -5,13 +5,18 @@
               [secretary.core :as secretary :include-macros true]
               [accountant.core :as accountant]))
 
+; lein clean && lein figwheel
+; (ns reagent-helloworld.core)
+
+; SKIP_BUILD=1 ./build.sh
+
 (comment
   (require '(reagent-helloworld.util)))
 
-(defn to-fixed [len i]
+(defn to-fixed "Add leading zeros to an integer" [len i]
   (let [i (str i)]
-    (str (clojure.string/join (repeat (- len (count i)) 0))
-         i)))
+    (-> (clojure.string/join (repeat (- len (count i)) "0"))
+        (str i))))
 
 (defn ns-to-str [tot]
   (when tot
@@ -31,8 +36,11 @@
    :stop-ts     (atom 0)})
 
 (defonce result-states
-  {:times       (atom []) ;(vec (for [_ (range 15)] (-> (Math/random) (* 5) (+ 20) (* 1e9) long))))
-   :result-avgs (atom (sorted-map 1 nil 5 nil 12 nil))})
+  {:times       (atom
+                  (case :sample
+                    :empty  []
+                    :sample (vec (for [_ (range 15)] (-> (Math/random) (* 5) (+ 20) (* 1e9) long)))))
+   :result-avgs (atom (sorted-map -1 nil 5 nil 12 nil))})
 
 (defn deref-states []
   (->> (for [s [timer-states result-states]]
@@ -40,16 +48,17 @@
        (apply merge)))
 
 (defn avg-of-n [n results]
-  (if (= n 1)
-    (last results)
-    (let [n-tot (count results)]
-      (when (>= n-tot n)
+  (let [n-tot (count results)]
+    (when (>= n-tot n)
+      (if (= n 1)
+        (nth results (dec n-tot))
         (->> (subvec results (- n-tot n) n-tot) sort rest butlast (apply +) double (* (/ (- n 2))))))))
 
 (comment
   (let [data [-10 2 30 4 5]]
     (for [i [1 3 5 10 12]]
       [i (avg-of-n i data)])))
+
 
 (def min-took-ns 2e9)
 
@@ -61,18 +70,30 @@
       (reset! stop-ts (reagent-helloworld.util/nanoTime))))
   
   
+  (defn reset-result-avgs!
+    ([] (some-> @times last reset-result-avgs!))
+    ([took-ns]
+     (let [times  @times
+           avgs   @result-avgs
+           n-avgs (keys avgs)]
+       (->> (zipmap n-avgs
+              (for [n n-avgs]
+                (if (= n -1)
+                  (if-let [current-best (avgs n)]
+                    (min current-best took-ns)
+                    took-ns)
+                  (avg-of-n n times))))
+            (into (sorted-map))
+            (reset! result-avgs)))))
+  
+  
   (defn conj-time! [ts]
     (reset! timer-state :stopped)
     (reset! stop-ts ts)
     (let [took-ns (- ts @start-ts)]
       (when (> took-ns min-took-ns)
-        (let [times  (swap! times conj took-ns)
-              avgs   @result-avgs
-              n-avgs (keys avgs)]
-          (->> (zipmap n-avgs (for [n n-avgs] (avg-of-n n times)))
-               (into (sorted-map))
-               (reset! result-avgs))))))
-            
+        (swap! times conj took-ns)
+        (reset-result-avgs! took-ns))))
   
   
   (let [tick-handle  (atom nil)
@@ -104,7 +125,7 @@
                 (reset! tick-handle (js/setInterval tick! 7))))
           
           [:started :down]
-          (do
+          (when (-> (- ts @start-ts) (> 0.2e9))
             (js/clearInterval @tick-handle)
             (reset! tick-handle nil)
             (conj-time! ts)))))))
@@ -113,16 +134,19 @@
 (comment
   (deref-states))
 
+(defonce _
+  (reset-result-avgs!))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (let [{:keys [timer-state down-ts start-ts stop-ts]} timer-states
       {:keys [times result-avgs]} result-states
 
-      spacebar? #(= (.-which %) 32)
+      spacebar?     #(= (.-which %) 32)
       make-event-fn #(fn[e] (when (% e) (click! %2)))
-      on-mouse-down (make-event-fn identity  :down)
-      on-mouse-up   (make-event-fn identity  :up)
-      on-key-down   (make-event-fn spacebar? :down)
-      on-key-up     (make-event-fn spacebar? :up)]
+      on-mouse-down  (make-event-fn identity  :down)
+      on-mouse-up    (make-event-fn identity  :up)
+      on-key-down    (make-event-fn spacebar? :down)
+      on-key-up      (make-event-fn spacebar? :up)]
   
   (defn home-page []
     (let [time-ns (- @stop-ts @start-ts)]
@@ -136,20 +160,20 @@
               :on-mouse-down  on-mouse-down :on-mouse-up  on-mouse-up
               :on-touch-start on-mouse-down :on-touch-end on-mouse-up
               :on-key-down    on-key-down   :on-key-up    on-key-up
-              :style {:color (cond
-                               (= @timer-state :pending) (if (= @down-ts 0) "green" "red")
-                               (= @timer-state :stopped) (if (or (= time-ns 0)
-                                                                 (> time-ns min-took-ns)) "black" "gray")
-                               :else "black")}}
+              :style {:color (case @timer-state
+                               :pending (if (= @down-ts 0) "green" "red")
+                               :stopped (if (or (= time-ns 0)
+                                                (> time-ns min-took-ns)) "black" "gray")
+                               "lightgray")}}
         [:h1 (ns-to-str time-ns)]]
        
        [:div {:id "app-bottombar"}
         (for [[k v] @result-avgs]
-          (let [id (str "app-bottombar-" k)]
+          (let [id (str "app-bottombar-" (Math/abs k))]
             ^{:key (str id "-" v)}
             [:span {:id id}
-             (str (case k 1 "BEST" (str "AVG " k)) " "
-                  (-> v ns-to-str (or "-")))]))]])))
+             (case k -1 "BEST" (str "AVG " k)) " "
+             [:b (-> v ns-to-str (or "-"))]]))]])))
 
 
 
